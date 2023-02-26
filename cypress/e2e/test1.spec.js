cf357340af4c84cd3fe453914c16d1b0543b9c63
tayyabsaleem <tayyabsaleem7756@gmail.com>
2023-02-26 18:17:32 +0600

    spav

--- cypress/e2e/test1.spec.js
+++ cypress/e2e/test1.spec.js
@@ -7,6 +7,7 @@ describe('Test Suite #1', () => {
   it('C2321: Verify that google page has input field', () => {
     //cy.visit('https://www.google.com/');
     //cy.get('input').should('be.visible');
+    //cy.get('input').should('be.visible');
   });
 
   
