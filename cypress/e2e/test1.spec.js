2841cc8e6f28a31a4b8e8a6e88e7c74f7085d1fb
tayyabsaleem <tayyabsaleem7756@gmail.com>
2023-02-27 22:30:36 +0500

    failed case

--- cypress/e2e/test1.spec.js
+++ cypress/e2e/test1.spec.js
@@ -5,8 +5,8 @@ describe('ignicube Demo: 1', () => {
   });
 
   it('C2321: Verify that google page has input field', () => {
-    //cy.visit('https://www.google.com/');
-    //cy.get('input').should('be.visible');
+    cy.visit('https://www.google.com/');
+    cy.get('inpcut').should('be.visible');
     //cy.get('input').should('be.visible');
   });
 
