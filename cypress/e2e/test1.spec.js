4534df56e59274cd4f521feb44f389a07b72418f
Tayyab Saleem <tayyabsaleem7756@gmail.com>
2023-06-01 12:29:13 +0500

    test

--- cypress/e2e/test1.spec.js
+++ cypress/e2e/test1.spec.js
@@ -1,14 +1,14 @@
 describe('ignicube Demo: 1', () => {
-  it('C2320: Verify that google page has input field', () => {
+  it('C2320: Verify that google page has input field y', () => {
     cy.visit('https://www.google.com/');
     cy.get('input').should('be.visible');
   });
 
   it('C2321: Verify that google page has input field', () => {
-   // cy.visit('https://www.google.com/');
+    // cy.visit('https://www.google.com/');
     //cy.get('inpcut').should('be.visible');
     //cy.get('input').should('be.visible');
   });
 
-  
+
 });
